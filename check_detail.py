e727b03c294c3146858495b55dec990deef7439c
Your Name <you@example.com>
2026-03-05 13:20:17 -0500

    Cleanup: remove temporary diagnostic scripts

--- check_detail.py
+++
@@ -1,6 +0,0 @@
-import traceback
-import jinja2
-try:
-    jinja2.Environment(loader=jinja2.FileSystemLoader('templates')).get_template('listing_detail.html')
-except Exception:
-    traceback.print_exc()
